62137759914729a7971988f65a4ec8259770365d
euna park <epark2746@columbia.edu>
2015-04-30 16:00:46 -0400

    js

--- js/main.js
+++ js/main.js
@@ -22,10 +22,10 @@
 
 	var map = L.map('map').setView([40.8075, -73.9619], 16);
 
-	// L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
- //    	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
- //    	maxZoom: 18
-	// }).addTo(map);
+	L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
+    	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    	maxZoom: 18
+	}).addTo(map);
 
  //    var marker = L.marker([40.8075, -73.9633]).addTo(map);
  //        marker.bindPopup("Columbia Journalism School");
